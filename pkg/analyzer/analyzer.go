6e56bf58eb810044cf3520b53e582c63304c2a21
Piotr Persona <persona.piotr@gmail.com>
2023-05-28 15:31:31 +0200

    refactor
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,45 +1,59 @@
 package analyzer
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/importer"
+	"go/token"
 	"go/types"
 
 	"golang.org/x/tools/go/analysis"
 )
 
-const lenFunctionName = "len"
+const (
+	lenFunctionName = "len"
+	arlenCmd        = "arlen"
+)
 
 var Analyzer = &analysis.Analyzer{
-	Name: "arlen",
+	Name: arlenCmd,
 	Doc:  "verifies if array len was checked before accessing the array",
 	Run:  run,
 }
 
-func run(pass *analysis.Pass) (interface{}, error) {
+func getTypesInfo(pass *analysis.Pass) (info *types.Info, err error) {
 	conf := types.Config{Importer: importer.Default()}
 
-	info := &types.Info{
+	if pass.Pkg == nil {
+		return nil, errors.New("package is nil")
+	}
+
+	info = &types.Info{
 		Defs:  make(map[*ast.Ident]types.Object),
 		Uses:  make(map[*ast.Ident]types.Object),
 		Types: make(map[ast.Expr]types.TypeAndValue),
 	}
 
-	_, err := conf.Check(pass.Pkg.Name(), pass.Fset, pass.Files, info)
+	pkgName := pass.Pkg.Name()
+	_, err = conf.Check(pkgName, pass.Fset, pass.Files, info)
 	if err != nil {
-		return nil, err
+		err = fmt.Errorf("cannot check package '%s, err: %w", pkgName, err)
 	}
-	objects := make(map[string]types.Object, len(info.Defs))
-	for ident, object := range info.Defs {
-		objects[ident.Name] = object
+	return
+}
+
+func run(pass *analysis.Pass) (i interface{}, err error) {
+	info, err := getTypesInfo(pass)
+	if err != nil {
+		return
 	}
 
-	arlenAnalyzer := NewArlenAnalyzer(pass, objects)
+	arlenAnalyzer := NewArlenAnalyzer(pass, info)
 	for _, f := range pass.Files {
 		ast.Inspect(f, arlenAnalyzer.Inspect)
 	}
-	return nil, nil
+	return
 }
 
 type ArlenAnalyzer struct {
@@ -48,7 +62,11 @@ type ArlenAnalyzer struct {
 	varLenChecked map[string]struct{}
 }
 
-func NewArlenAnalyzer(pass *analysis.Pass, objects map[string]types.Object) *ArlenAnalyzer {
+func NewArlenAnalyzer(pass *analysis.Pass, info *types.Info) *ArlenAnalyzer {
+	objects := make(map[string]types.Object, len(info.Defs))
+	for ident, object := range info.Defs {
+		objects[ident.Name] = object
+	}
 	return &ArlenAnalyzer{
 		varLenChecked: make(map[string]struct{}, 0),
 		pass:          pass,
@@ -61,14 +79,14 @@ func (a *ArlenAnalyzer) Inspect(node ast.Node) (procceed bool) {
 
 	switch stmt := node.(type) {
 	case *ast.IndexExpr:
-		a.verifyIfVariableWasChecked(stmt)
+		a.verifyArrayCheck(stmt)
 	case *ast.IfStmt:
-		a.registerCheckedVariables(stmt)
+		a.registerArrayCheck(stmt)
 	}
 	return
 }
 
-func (a *ArlenAnalyzer) verifyIfVariableWasChecked(expr *ast.IndexExpr) {
+func (a *ArlenAnalyzer) verifyArrayCheck(expr *ast.IndexExpr) {
 	ident, ok := expr.X.(*ast.Ident)
 	if !ok {
 		return
@@ -86,46 +104,58 @@ func (a *ArlenAnalyzer) verifyIfVariableWasChecked(expr *ast.IndexExpr) {
 		return
 	}
 
-	if _, ok := a.varLenChecked[a.identKey(ident)]; ok {
+	if _, ok := a.varLenChecked[identID(ident)]; ok {
 		return
 	}
-	a.pass.Reportf(ident.Pos(), "arlen: check variable %s before accessing", name)
+	artype := "slice"
+	if isArray {
+		artype = "array"
+	}
+	a.report(ident.Pos(), "check %s %s length before accessing", artype, name)
 }
 
-func (a *ArlenAnalyzer) registerCheckedVariables(stmt *ast.IfStmt) {
+func (a *ArlenAnalyzer) report(pos token.Pos, format string, args ...any) {
+	a.pass.Reportf(pos, "%s: %s", arlenCmd, fmt.Sprintf(format, args...))
+}
+
+func (a *ArlenAnalyzer) registerArrayCheck(stmt *ast.IfStmt) {
 	binaryExpr, ok := stmt.Cond.(*ast.BinaryExpr)
 	if !ok {
 		return
 	}
-	lenExpr := a.getLenExpression(binaryExpr)
+	lenExpr := a.getLenCallExpr(binaryExpr)
 	if lenExpr == nil {
 		return
 	}
 	for _, arg := range lenExpr.Args {
 		if ident, ok := arg.(*ast.Ident); ok {
-			a.varLenChecked[a.identKey(ident)] = struct{}{}
+			a.varLenChecked[identID(ident)] = struct{}{}
 		}
 	}
 }
 
-func (a *ArlenAnalyzer) getLenExpression(binaryExpr *ast.BinaryExpr) (expr *ast.CallExpr) {
-	getCallExpr := func(expr ast.Expr) *ast.CallExpr {
-		callExpr, ok := expr.(*ast.CallExpr)
-		if !ok {
-			return nil
-		}
-		if fnIdent, ok := callExpr.Fun.(*ast.Ident); ok && fnIdent.Name != lenFunctionName {
-			return nil
-		}
-		return callExpr
-	}
-
-	if expr := getCallExpr(binaryExpr.X); expr != nil {
+func (a *ArlenAnalyzer) getLenCallExpr(binaryExpr *ast.BinaryExpr) (expr *ast.CallExpr) {
+	if expr := a.getLenExpr(binaryExpr.X); expr != nil {
 		return expr
 	}
-	return getCallExpr(binaryExpr.Y)
+	return a.getLenExpr(binaryExpr.Y)
 }
 
-func (a *ArlenAnalyzer) identKey(ident *ast.Ident) string {
+func (a *ArlenAnalyzer) getLenExpr(expr ast.Expr) *ast.CallExpr {
+	callExpr, ok := expr.(*ast.CallExpr)
+	if !ok {
+		return nil
+	}
+	if fnIdent, ok := callExpr.Fun.(*ast.Ident); ok && fnIdent.Name != lenFunctionName {
+		return nil
+	}
+	return callExpr
+}
+
+func identID(ident *ast.Ident) string {
 	return fmt.Sprintf("%s_%d", ident.Name, ident.Obj.Pos())
+}
+
+func exprID(expr ast.Expr) string {
+	return fmt.Sprintf("%d_%d", expr.Pos(), expr.End())
 }
